b65a0970915da4ee7d15150f0043cf9918a19510
BriceNogue <bricenogue@gmail.com>
2024-01-29 10:54:05 +0100

    .

--- obj/Debug/net6.0/BlazingPizza.AssemblyInfo.cs
+++ obj/Debug/net6.0/BlazingPizza.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazingPizza")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0855900f357d07f599d6518a2d048e1a68e4e9e6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aeac9c4dc5d3b0c9fb68a65f7c35a4b35ebd3aca")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazingPizza")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazingPizza")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
